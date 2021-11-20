01e75fb218e1f325c2d5534744f19001fdddb6ee
Selfmotivation <mira55@yandex.ru>
2021-11-20 04:12:48 +0400

    add: eslint, githubActions, codeClimate

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}`);
+function greeting() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+}
 
-export default name;
+export default greeting;
